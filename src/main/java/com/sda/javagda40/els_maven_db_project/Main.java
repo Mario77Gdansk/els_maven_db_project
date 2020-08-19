4ad1747e8510e23379211a27250ef688d77bb9f7
Mariusz <mariusz@kursGIT.pl>
2020-08-19 23:34:34 +0200

    Initial commit

---
+++ src/main/java/com/sda/javagda40/els_maven_db_project/Main.java
@@ -0,0 +1,9 @@
+package com.sda.javagda40.els_maven_db_project;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Initial version.");
+        }
+}
